7220c2d23617e7449cff078bab40044961f2a2e2
EMMANUEL AYOBAMI ADEWUMI <135729444+SCCSMARTCODE@users.noreply.github.com>
2024-10-25 16:48:38 +0100

    Time: 0 ms (100%), Space: 17.3 MB (17.5%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -0,0 +1,14 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+
+        while left <= right:
+            center = (left + right) // 2
+
+            if nums[center] > target:
+                right = center - 1
+            elif nums[center] < target:
+                left = center + 1
+            else:
+                return center
+        return left
