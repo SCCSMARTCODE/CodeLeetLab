9878c0edd91b56fe724b5ab11f527c7c3ae2d9be
EMMANUEL AYOBAMI ADEWUMI <135729444+SCCSMARTCODE@users.noreply.github.com>
2025-01-28 09:17:49 +0100

    Time: 83 ms (46.99%), Space: 49.2 MB (5.23%) - LeetHub

--- 2236-maximum-twin-sum-of-a-linked-list/2236-maximum-twin-sum-of-a-linked-list.py
+++ 2236-maximum-twin-sum-of-a-linked-list/2236-maximum-twin-sum-of-a-linked-list.py
@@ -3,20 +3,29 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution:
-    def pairSum(self, head: Optional[ListNode]) -> int:
-        if not head or not head.next:
-            return 0
 
-        dq = deque()
-        current_node = head
+class Solution:  
+    def pairSum(self, head: Optional[ListNode]) -> int:  
+        if not head or not head.next:  
+            return 0  
 
-        while current_node is not None:
-            dq.append(current_node.val)
-            current_node = current_node.next
+        length = 0  
+        current_node = head  
+        while current_node:  
+            length += 1  
+            current_node = current_node.next  
 
-        max_sum = float('-inf')
-        for _ in range(len(dq) // 2):
-            max_sum = max(max_sum, dq.pop() + dq.popleft())
+        mid = length // 2  
+        current_node = head  
+        
+        stack = []  
+        for _ in range(mid):  
+            stack.append(current_node.val)  
+            current_node = current_node.next  
+            
+        max_sum = float('-inf')  
+        for _ in range(mid):  
+            max_sum = max(max_sum, stack.pop() + current_node.val)  
+            current_node = current_node.next  
 
         return max_sum 
